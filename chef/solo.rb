c214aad6da75661bd69541062937a9f0725d178b
Saravana Kumar Periyasamy <saravanakumar.periyasamy@gmail.com>
2015-08-24 22:42:06 -0500

    added the environment variable to solo.rb

--- chef/solo.rb
+++ chef/solo.rb
@@ -2,5 +2,6 @@ file_cache_path "/var/chef/cache"
 file_backup_path "/var/chef/backup"
 cookbook_path "/opt/chef/cookbooks"
 role_path "/opt/chef/roles"
+environment_path "/opt/chef/environments"
 log_level :info
 verbose_logging false
--- jenkins/python/petclinic_cfn.py
+++ jenkins/python/petclinic_cfn.py
@@ -2,7 +2,7 @@ import boto
 import boto.cloudformation
 
 
-template_body = open('/var/lib/jenkins/jobs/petclinic-aws-deploy/workspace/cloudformation/petclinic.json').read()
+template_body = open('cloudformation/petclinic.json').read()
 print template_body
 cfn_conn = boto.cloudformation.connect_to_region('us-west-1')
 cfn_conn.create_stack('petclinic', template_body)
