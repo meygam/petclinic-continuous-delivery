552b6703de7e27f0802e3e7134a9e98681d1565e
Saravana Kumar Periyasamy <saravanakumar.periyasamy@gmail.com>
2015-08-25 00:20:13 -0500

    removed the release repo

--- chef/cookbooks/meygam_petclinic/attributes/default.rb
+++ chef/cookbooks/meygam_petclinic/attributes/default.rb
@@ -1,7 +1,6 @@
 default['meygam']['artifactory_server'] = '192.168.33.10'
 default['meygam']['artifactory_repos'] = [
-  "http://#{node['meygam']['artifactory_server']}:8081/artifactory/libs-snapshot-local",
-  "http://#{node['meygam']['artifactory_server']}:8081/artifactory/libs-release-local"
+  "http://#{node['meygam']['artifactory_server']}:8081/artifactory/libs-snapshot-local"
 ]
 
 default['meygam_petclinic']['java_max_memory'] = '1024m'
